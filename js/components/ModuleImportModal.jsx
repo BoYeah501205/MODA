f8f521c37cc3aae3ca99c5568edafe606814338a
Trevor Fletcher <trevor@OFFICE.local>
2026-01-19 17:34:13 -0700

    Add ModuleImportModal.jsx with global React pattern (fixes useState error)

---
+++ js/components/ModuleImportModal.jsx
@@ -0,0 +1,11 @@
+/***a Conen
+* Hns importo mdules withnlysi an vrificatn sep
+ */const { usStae } =React;
+
+ {
+    // Get import functionsfrom global
+    const  analyzeModuleImport, executeModuleImport, parseModuleCSV } = window.MODA_MODULE_IMPORT || {};
+    
+
+// Expose globally for use in App.jsx
+window.ModuleImportModal = ModuleImportModal;
